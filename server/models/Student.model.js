654f482fb81b732551be1179a837381e51add6ad
Tanja Mueller <tmueller19@gmx.de>
2026-01-10 23:04:55 +0100

    Day 2 complete

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -9,16 +9,12 @@ const studentSchema = new Schema({
     email: { type: String, unique: true },
     phone: { type: String },
     linkedinUrl: { type: String },
-    languages: [{ type: String, enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"] }],
+    languages: { type: [String], enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"] },
     program: { type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"] },
     background: { type: String },
-    image: { type: String },
-    projects: {
-        type: Array,
-         cohort: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Cohort"
-        } }
+    image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
+    projects: { type: [Schema.Types.ObjectId] },
+    cohort: { type: Schema.Types.ObjectId, ref: "Cohort"}
 })
 
 const Student = mongoose.model("Student", studentSchema);
